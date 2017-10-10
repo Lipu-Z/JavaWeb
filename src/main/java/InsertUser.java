223ed3a05a7dd232fb3444283dc9d5808a5be2bc
lzhong <lzhong@systemobjects.com>
2017-10-10 16:52:30 +0200

    add 2nd page and servlet, realise funstion of insert to the sql

--- src/main/java/InsertUser.java
+++ src/main/java/InsertUser.java
@@ -4,30 +4,29 @@ import javax.servlet.http.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class main extends HttpServlet{
-    protected void doGet(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
+
+public class InsertUser extends HttpServlet {
+    protected void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
         String username=request.getParameter("username");
         String password=request.getParameter("password");
-        System.out.println("username=" + username);
-        System.out.println("password=" + password);
-        boolean login = false;
+        boolean insert = false;
         try {
-            login = login(username,password);
+            insert = Insert(username,password);
         } catch (Exception e){
             e.printStackTrace();
         }
-        if(login) {
+        if(insert) {
             response.setContentType("text/html");
-            response.getWriter().println("Login Success!!!");
+            response.getWriter().println("Insert Sucess!!!");
         } else {
-            response.sendRedirect("insertuser.jsp");
+            response.setContentType("text/html");
+            response.getWriter().println("Insert Failed!!!");
         }
     }
 
-    public boolean login(String username,String password) throws ClassNotFoundException, SQLException{
+    public boolean Insert(String username,String password) throws ClassNotFoundException, SQLException{
             /*
              * 用户登录方法
              */
@@ -36,26 +35,24 @@ public class main extends HttpServlet{
         //2.获取连接
         Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","lip","1049");
         //3.编写sql语句
-        String sql="select * from login where Name=? and PWD=?";
+        String sql="insert into login(Name,PWD) values(?,?)";
         //4.创建预处理对象
         PreparedStatement pstmt=conn.prepareStatement(sql);
         //5.设置参数（给占位符）
         pstmt.setString(1, username);
         pstmt.setString(2, password);
         //6.执行查询操作
-        ResultSet rs=pstmt.executeQuery();
+        int row =pstmt.executeUpdate();
         //7.对结果集进行处理
-        if(rs.next()){
-            if(rs!=null) rs.close();
+        if(row!=0){
             if(pstmt!=null) pstmt.close();
             if(conn!=null) conn.close();
             return true;
-        }else{
-            if(rs!=null) rs.close();
+        } else{
             if(pstmt!=null) pstmt.close();
             if(conn!=null) conn.close();
             return false;
         }
-
     }
+
 }
---
+++ target/mavenweb/insertuser.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LZHONG
+  Date: 10/10/2017
+  Time: 4:12 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Insert</title>
+</head>
+<body>
+<form action="insertServlet" method="post">
+    User:<input type="text" name="username"><br>
+    PWD:<input type="password" name="password"><br>
+    <input type="submit" value="insert">
+</form>
+</body>
+</html>
